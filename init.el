67279b1a70dc0deb51befbe98a5fd4ecaf95fd94
Angad Singh <angadsingh567890@gmail.com>
2018-11-12 01:41:19 +0530

    Random changes

--- init.el
+++ init.el
@@ -1,20 +1,2 @@
+;; A one-liner (not quite)!
 (org-babel-load-file "~/.emacs.d/README.org")
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "bf390ecb203806cbe351b966a88fc3036f3ff68cd2547db6ee3676e87327b311" default)))
- '(package-selected-packages
-   (quote
-    (org-bullets eshell-git-prompt exec-path-from-shell smart-mode-line powerline nord-theme magit counsel ivy projectile flycheck smartparens ranger iedit undo-tree expand-region ace-window which-key try use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
