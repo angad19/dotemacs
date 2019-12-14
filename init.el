5428f6a2a0a9129699f242ab4dfea84ca1ae0677
Angad Singh <mail@angad.dev>
2019-12-14 11:24:33 +0530

    Minor updates

--- init.el
+++ init.el
@@ -1,2 +1,16 @@
 ;; A one-liner (not quite)!
 (org-babel-load-file "~/.emacs.d/README.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (cyberpunk-theme cyberpunk org-bullets elfeed-goodies elfeed go-eldoc company-go go-mode css-eldoc restclient pug-mode markdown-mode emmet-mode web-mode prettier-js add-node-modules-path json-mode rjsx-mode indium js2-mode php-mode yaml-mode textile-mode elpy company-jedi company eshell-git-prompt exec-path-from-shell smart-mode-line magit counsel ivy projectile flycheck hl-todo smartparens iedit undo-tree expand-region ace-window which-key dimmer simpleclip try use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
