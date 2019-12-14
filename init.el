bb36be9f402a2d0e7878f4a03b0b2c20d6f580a1
Angad Singh <mail@angad.dev>
2019-12-14 13:03:59 +0530

    Use doom-modeline, other minor changes

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (cyberpunk-theme cyberpunk org-bullets elfeed-goodies elfeed go-eldoc company-go go-mode css-eldoc restclient pug-mode markdown-mode emmet-mode web-mode prettier-js add-node-modules-path json-mode rjsx-mode indium js2-mode php-mode yaml-mode textile-mode elpy company-jedi company eshell-git-prompt exec-path-from-shell smart-mode-line magit counsel ivy projectile flycheck hl-todo smartparens iedit undo-tree expand-region ace-window which-key dimmer simpleclip try use-package))))
+    (doom-modeline all-the-icons org-bullets go-eldoc company-go go-mode css-eldoc restclient pug-mode markdown-mode emmet-mode web-mode prettier-js add-node-modules-path json-mode rjsx-mode indium js2-mode php-mode yaml-mode textile-mode elpy company-jedi company eshell-git-prompt exec-path-from-shell spaceline cyberpunk-theme magit counsel ivy projectile flycheck evil-surround evil key-chord hl-todo smartparens iedit undo-tree expand-region ace-window which-key dimmer simpleclip try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
